53d1f31fc41e595182b58a5cb3c5affeb1a2d834
Sirajudin Badi <sirubadi79@gmail.com>
2025-08-25 18:32:25 +0530

    [NEW]-New module for PDF Report Tasks

---
+++ v18/report_generator/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name" : "Report Generator",
+    "description" : "Custom PDF Report Generator.",
+    "summary" : """This module will generate custom QWeb Reports.""",
+    "installable" : True,
+    "application" : True,
+    "depends" : ["base", "web", "sale",],
+    "license" : "LGPL-3",
+    "version" : "18.0.0.1",
+    "data" : [],
+}
---
+++ v18/report_generator/models/res_partner.py
@@ -0,0 +1,22 @@
+# coding:utf-8-*-
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    def get_customers(self):
+        customers = self.env['res.partner'].sudo().search([
+            ("customer_rank", '>', 0),
+            ("active", '=', True),
+        ])
+
+        return customers
+
+    def generate_customer_pdf(self):
+        customers = self.get_customers()
+        if not customers:
+            raise ValidationError("No customers exist.")
+        return self.env.ref("report_generator.report_customer_action").report_action(customers)
