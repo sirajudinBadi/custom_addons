91fc55f7725b918f95ff20ea00ccb4a65cd0bb8b
Sirajudin Badi <sirubadi79@gmail.com>
2025-08-25 17:36:58 +0530

    [ADD]-Email restriction for Log Note

--- v18/email_remainder/models/sale_order.py
+++ v18/email_remainder/models/sale_order.py
@@ -2,7 +2,7 @@
 
 from odoo import models, api
 from odoo.exceptions import ValidationError
-
+import re
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -11,14 +11,41 @@ class SaleOrder(models.Model):
     def _notify_get_recipients(self, message, msg_vals, **kwargs):
         recipients = super()._notify_get_recipients(message, msg_vals, **kwargs)
 
-        partners = []
+        # get subtype safely
+        subtype_id = msg_vals.get("subtype_id") if msg_vals else message.subtype_id.id
+        body = msg_vals.get("body") if msg_vals else (message.body or "")
+
+        # extract mentioned partners
+        mentioned_partner_ids = set(map(int, re.findall(r'id=(\d+)&model=res\.partner', body)))
+
+        allowed_recipients = []
+
         for r in recipients:
             partner = self.env['res.partner'].browse(r['id'])
-            if partner.restrict_email:
+
+            if subtype_id == 1:  # Send Message
+                if not partner.restrict_email:
+                    allowed_recipients.append(r)
+                # restricted → skipped
                 continue
+
+            elif subtype_id == 2:  # Log Note
+                print("Log NOTE")
+                if partner.restrict_email:
+                    print("Inside lognote Restricted partner")
+
+                    # allow mail but prevent auto-follow
+                    r['is_follower'] = False
+                    print(f"Reached")
+                    allowed_recipients.append(r)
+                    # else restricted & not mentioned → skip
+                else:
+                    r["is_follower"] = True
+                    allowed_recipients.append(r)
+                continue
+
             else:
-                partners.append(r)
+                # fallback (other subtypes)
+                allowed_recipients.append(r)
 
-        recipients = partners
-
-        return recipients
+        return allowed_recipients
